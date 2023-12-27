acfd8003e0306f2093f1df24a6d7d7a43818b94b
krizhnaa <krizhnaprem@gmail.com>
2023-12-27 17:15:05 +0530

    Done with the 4th Obj

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 # 2. By using Canvas widget fitting the tomato image into Tkinter window
 
 # 3. Creating a text which displays the timer inside the Tomato pic & Adding the background color.
+
+# 4. Creating a label called "timer", two buttons and checkmark for pomodore completion.
--- main.py
+++ main.py
@@ -21,11 +21,27 @@ window = Tk()
 window.title("Pomodore Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+label = Label()
+label.config(text="Timer", font=(FONT_NAME, 28, "bold"), bg=YELLOW, foreground=GREEN)
+label.grid(row=0, column=1)
+
+label1 = Label()
+label1.config(text="✓", font=(FONT_NAME, 12, "bold"), bg=YELLOW, foreground=GREEN)
+label1.grid(row=3, column=1)
+
+start = Button()
+start.config(text="Start")
+start.grid(row=2, column=0)
+
+reset = Button()
+reset.config(text="Reset")
+reset.grid(row=2, column=2)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tom_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tom_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
 
 
